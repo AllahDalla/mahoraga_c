762c85348f0876bd71af203e7f6438a5523791ab
AllahDalla <alromariodavis@gmail.com>
2025-03-16 23:52:30 -0500

    Adding attack squares for each piece - curr: rook

---
+++ bbc.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+
+// define bitboard data type
+#define u64 unsigned long long
+
+// board squares
+enum{
+
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1,
+};
+
+enum {white, black};
+
+
+/* 
+    ********************************************
+    *
+    *               BIT MANIPULATION
+    * 
+    * 
+    ******************************************** 
+*/
+
+// bit macros
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+#define set_bit(bitboard, square) (bitboard |= (1ULL << square))
+#define pop_bit(bitboard, square) (get_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0)
+
+
+void print_bitboard(u64 bitboard){
+    // loop over ranks
+    printf("\n");
+    for(int rank = 0; rank < 8; rank++){
+        // loop over files
+        for(int file = 0; file < 8; file++){
+            // get square index
+            int square = rank * 8 + file;
+            // get bit at square
+            u64 bit = get_bit(bitboard, square) ? 1 : 0;
+            // print bit
+
+            if(!file){
+                printf("%d ", 8 - rank);
+            }
+
+            printf(" %llu", bit);
+        }
+        printf("\n");
+    }
+
+    printf("\n   a b c d e f g h\n");
+
+    // unsigned long long decimal of bitboard
+    printf("\n  bitboard: %llu decimal\n", bitboard);
+}
+
+/* 
+    ********************************************
+    *
+    *               ATTACKS
+    * 
+    * 
+    ******************************************** 
+*/
+// constant for Not A File
+const u64 not_a_file = 18374403900871474942ULL;
+// constant for Not H File
+const u64 not_h_file = 9187201950435737471ULL;
+// constant for not HG File
+const u64 not_hg_file = 4557430888798830399ULL;
+// constant for not AB File
+const u64 not_ab_file = 18229723555195321596ULL;
+
+u64 pawn_attacks[2][64];
+u64 knight_attacks[64];
+u64 king_attacks[64];
+u64 bishop_masks[64];
+
+/**
+ * Generates a bitboard representing pawn attack squares for a given side and square.
+ *
+ * @param side Color of the pawn (0 for white, 1 for black)
+ * @param square The starting square of the pawn
+ * @return A bitboard representing the squares the pawn can attack
+ */
+u64 mask_pawn_attacks(int side, int square){
+    u64 attacks = 0ULL;
+    u64 bitboard = 0ULL; 
+
+    set_bit(bitboard, square);
+
+    // for white pawns
+    if(!side){
+        if((bitboard >> 7) & not_a_file){
+            attacks |= (bitboard >> 7);
+        }
+
+        if((bitboard >> 9) & not_h_file){
+            attacks |= (bitboard >> 9);
+        }
+    }else{
+        // for black pawns
+        if((bitboard << 7) & not_h_file){
+            attacks |= (bitboard << 7);
+        }
+
+        if((bitboard << 9) & not_a_file){
+            attacks |= (bitboard << 9);
+        }
+    }
+
+    return attacks;
+}
+
+u64 mask_knight_attacks(int square){
+    u64 attacks = 0ULL;
+    u64 bitboard = 0ULL;
+    set_bit(bitboard, square);
+
+    if((bitboard >> 17) & not_h_file) attacks |= (bitboard >> 17);
+    if((bitboard >> 15) & not_a_file) attacks |= (bitboard >> 15);
+    if((bitboard >> 10) & not_hg_file) attacks |= (bitboard >> 10);
+    if((bitboard >> 6) & not_ab_file) attacks |= (bitboard >> 6);
+    if((bitboard << 17) & not_a_file) attacks |= (bitboard << 17);
+    if((bitboard << 15) & not_h_file) attacks |= (bitboard << 15);
+    if((bitboard << 10) & not_ab_file) attacks |= (bitboard << 10);
+    if((bitboard << 6) & not_hg_file) attacks |= (bitboard << 6);
+
+    return attacks;
+
+}
+
+u64 mask_king_attacks(int square){
+    u64 attacks = 0ULL;
+    u64 bitboard = 0ULL;
+    set_bit(bitboard, square);
+
+    if((bitboard >> 9) & not_h_file) attacks |= (bitboard >> 9);
+    if(bitboard >> 8) attacks |= (bitboard >> 8);
+    if((bitboard >> 7) & not_a_file) attacks |= (bitboard >> 7);
+    if((bitboard >> 1) & not_h_file) attacks |= (bitboard >> 1);
+
+    if((bitboard << 9) & not_a_file) attacks |= (bitboard << 9);
+    if(bitboard << 8) attacks |= (bitboard << 8);
+    if((bitboard << 7) & not_h_file) attacks |= (bitboard << 7);
+    if((bitboard << 1) & not_a_file) attacks |= (bitboard << 1);
+
+    return attacks;
+}
+
+u64 mask_bishop_attacks(int square){
+    u64 attacks = 0ULL;
+    u64 bitboard = 0ULL;
+    set_bit(bitboard, square);
+
+    int rank, file;
+    int target_rank = square / 8;
+    int target_file = square % 8;
+
+    for(rank = target_rank + 1, file = target_file + 1; rank <= 6 && file <= 6; rank++, file++){
+        attacks |= (1ULL << (rank * 8 + file));
+    }
+    for(rank = target_rank - 1, file = target_file + 1; rank >= 1 && file <= 6; rank--, file++){
+        attacks |= (1ULL << (rank * 8 + file));
+    }
+
+    for(rank = target_rank + 1, file = target_file - 1; rank <= 6 && file >= 1; rank++, file--){
+        attacks |= (1ULL << (rank * 8 + file));
+    }
+
+    for(rank = target_rank - 1, file = target_file - 1; rank >= 1 && file >= 1; rank--, file--){
+        attacks |= (1ULL << (rank * 8 + file));
+    }
+
+    return attacks;
+}
+
+void init_pawn_attacks(){
+    for(int square = 0; square < 64; square++){
+        // pawn attacks
+        pawn_attacks[white][square] = mask_pawn_attacks(white, square);
+        pawn_attacks[black][square] = mask_pawn_attacks(black, square);
+
+        // knight attacks
+        knight_attacks[square] = mask_knight_attacks(square);
+
+        // king attacks
+        king_attacks[square] = mask_king_attacks(square);
+    }
+}
+
+
+/* 
+    ********************************************
+    *
+    *               MAIN DRIVER
+    * 
+    * 
+    ******************************************** 
+*/
+
+int main(){
+    init_pawn_attacks(); 
+    
+    print_bitboard(mask_bishop_attacks(d5));
+
+    return 0;
+}
